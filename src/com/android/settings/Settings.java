6b9333d711d4a2e3d6326e36d4fd2167fbc99384
Jorrit Jongma <git@jongma.org>
2014-01-23 23:47:49 -0500

    Settings: SuperSU from Settings
    If SuperSU is installed, show an entry in Settings below
    "Developer options", which launches SuperSU when tapped.
    At boot time SuperSU will detect if com.android.settings
    contains the resources from this patch, and if so it
    will hide itself from the launcher. This was implemented
    in v1.85, so we only show the entry if the installed
    SuperSU version is at least that.
    
    PS2: Better Drawables courtesy of Lo
    
    Signed-off-by: ayysir <dresadd09691@gmail.com>
    Signed-off-by: houst0nn <houstonn@gmail.com>
    Change-Id: I1796d92458587b9e961d5d8e6d58099e8367834d
    
    Conflicts:
    	src/com/android/settings/Settings.java

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -802,6 +802,16 @@ public class Settings extends PreferenceActivity
                 if(!Utils.isPackageInstalled(this, VOICE_WAKEUP_PACKAGE_NAME)) {
                     target.remove(header);
                 }
+            } else if (id == R.id.supersu_settings) {
+                // Embedding into Settings is supported from SuperSU v1.85 and up
+                boolean supported = false;
+                try {
+                    supported = (getPackageManager().getPackageInfo("eu.chainfire.supersu", 0).versionCode >= 185);
+                } catch (PackageManager.NameNotFoundException e) {
+                }
+                if (!supported) {
+                    target.remove(i);
+                }
             }
 
             if (i < target.size() && target.get(i) == header
