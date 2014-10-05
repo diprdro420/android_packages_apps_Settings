af1bb6c5ae86a5f9c7d1abd8a74fb6995ce06cf0
rogersb11 <brettrogers11@gmail.com>
2014-10-04 23:43:53 -0400

    Rearrange settings, new menu additions

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -210,10 +210,8 @@ public class Settings extends PreferenceActivity
             R.id.home_settings,
             R.id.interface_section,
             R.id.status_bar_settings,
-            R.id.notification_bar_settings,
             R.id.lock_screen_settings,
-            R.id.privacy_settings_cyanogenmod,
-            R.id.button_settings
+            R.id.privacy_settings_cyanogenmod
     };
 
     private SharedPreferences mDevelopmentPreferences;
